2d690f92f2435c2ac3ea1ab85244f135ad995146
gabriel <gsf.ferreira99@gmail.com>
2022-10-11 04:52:12 +0000

    update

---
+++ static/js/table.0fb84b9e56da.js
@@ -0,0 +1,13 @@
+tab = $(document).ready( function () {
+    table = $('#tabela').DataTable(
+        {
+            "language": {
+                'url': '//cdn.datatables.net/plug-ins/1.11.5/i18n/pt-BR.json'
+            },
+            scrollX : true
+        }
+    );
+} );
+
+
+
