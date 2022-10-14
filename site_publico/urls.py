af8a38cebcb5a787668687b39b82dc04591da745
Gabriel <gsf.ferreira99@gmail.com>
2022-10-13 21:54:32 -0400

    update produtos

--- site_publico/urls.py
+++ site_publico/urls.py
@@ -4,4 +4,6 @@ from site_publico import views
 
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
+    path('produtos/', views.Produtos.as_view(), name='produtos'),
+    path('produtos/<pk>', views.ProdutoDetalhes.as_view(), name='produto-detalhes'),
 ]
--- site_publico/views.py
+++ site_publico/views.py
@@ -7,3 +7,15 @@ class Index(View):
 
     def get(self, request):
         return render(request, 'index.html')
+
+
+class Produtos(View):
+
+    def get(self, request):
+        return render(request, 'produtos.html')
+
+
+class ProdutoDetalhes(View):
+
+    def get(self, request, pk):
+        return render(request, 'produto-detalhes.html')
